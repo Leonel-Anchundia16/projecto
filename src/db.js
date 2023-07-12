a31487bc8aa1a78e12d9e096e15bba7ee29a928e
unknown <leonelanchundia2000@gmail.com>
2023-07-12 16:13:44 -0500

    major: 002

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import { MONGODB_URI } from "./config";
 export const connectDB = async () => {
   try {
     await connect(MONGODB_URI);
-    console.log("Connected to db");
+    console.log("Base de datos escuchando");
   } catch (error) {
     console.error(error);
   }
